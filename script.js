335fab63f80dc07a6fe17d5771373ff1a18034de
Robert Drabant <rdrabant@resultsdelivered.com>
2021-09-28 19:31:24 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let localStore = window.localStorage;
+
+let doggoVotes = localStore.getItem('doggo');
+let catVotes = localStore.getItem('cat')
+let fishVotes = localStore.getItem('fish')
+
+if(doggoVotes === null){
+    localStore.setItem('doggo', 0);
+}
+
+if(catVotes === null){
+    localStore.setItem('cat', 0);
+}
+
+if(fishVotes === null){
+    localStore.setItem('fish', 0);
+}
+
+function voteDoggo(){
+    console.log("doggo vote called");
+}
